1ad4daa005cd0df543fd66ab3afd1b4c59a012ea
Nadiaa03 <nadia.excelintern@gmail.com>
2024-08-09 21:45:58 -0400

    Delete databasesetup directory

--- databasesetup/routes/RouteLike.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const likeController = require('../controllers/ControllerLike');
-
-// Define routes for like actions
-router.post('/', likeController.likePost);
-router.delete('/:id', likeController.unlikePost);
-
-module.exports = router;
