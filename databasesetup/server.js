cb3c8b1572ce54072058bffd33f247b6a740b778
Nadiaa03 <nadia.excelintern@gmail.com>
2024-08-28 17:42:47 -0400

    Update server.js

--- databasesetup/server.js
+++ databasesetup/server.js
@@ -19,14 +19,14 @@ const userRoutes = require('./routes/RouteUser');
 const postRoutes = require('./routes/RoutePosts');
 const commentRoutes = require('./routes/RouteComment');
 const likeRoutes = require('./routes/RouteLike');
-const followRoutes = require('./routes/RouteFollow'); // Add this line
+const connectionRoutes = require('./routes/RouteConnection');
 
 // Use routes
 app.use('/users', userRoutes);
 app.use('/posts', postRoutes);
 app.use('/comments', commentRoutes);
 app.use('/likes', likeRoutes);
-app.use('/follows', followRoutes); // Add this line
+app.use('/api/connections', connectionRoutes);
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
