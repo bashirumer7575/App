e7c41416dabbfb76ca28bfa46727116bb262c5f5
Nadiaa03 <nadia.excelintern@gmail.com>
2024-08-28 18:35:21 -0400

    Update User.js

--- databasesetup/models/User.js
+++ databasesetup/models/User.js
@@ -1,13 +1,11 @@
-// models/User.js
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now },
-    following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-    followers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
+  username: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+  connections: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }], // Replacing following and followers
 });
 
 const User = mongoose.model('User', userSchema);
